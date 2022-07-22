ceac4c62330f1c5642549d152dc3042a081ec664
chouran <chouran.c@gmail.com>
2022-07-21 23:33:26 -0400

    add console logger

--- app/question_answering/main.py
+++ app/question_answering/main.py
@@ -1,4 +1,4 @@
-#from app.utils import common_utils as utils
+from utils import common_utils as utils
 import pickle
 
 def test_import():
--- app/utils/common_utils.py
+++ app/utils/common_utils.py
@@ -5,6 +5,16 @@ import sys
 
 performance_log = {}
 
+#Creating and Configuring Logger
+def console_logger(module_name):
+    Log_Format = "[%(asctime)s] [%(levelname)s] ["+ module_name + "] - %(message)s"
+    logging.basicConfig(stream = sys.stdout, 
+                        filemode = "w",
+                        format = Log_Format, 
+                        level = service_info["log_level"])
+    logger = logging.getLogger()
+    return logger
+    
 # decorator to calculate process computation duration
 def calculate_duration(func):
     def inner_method(*args, **kwargs):
